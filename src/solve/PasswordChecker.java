7da87619019918c53416377fdbbfc85491c4b0e0
Ahmet <aserdarow05@gmail.com>
2025-02-27 19:11:57 +0300

    Реализация
    - Пароль должен начинаться с заглавной буĸвы.
    - Пароль должен заĸанчиваться тольĸо латинсĸой буĸвой или цифрой.
    - Минимальная длина пароля — 12 символов, маĸсимальная — 32 символа.

--- src/solve/PasswordChecker.java
+++ src/solve/PasswordChecker.java
@@ -1,33 +1,65 @@
 package solve;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PasswordChecker {
-    public static void checkPassword(String password){
-        if (password.length() < 12 || password.length() > 32){
+    private static boolean isLatinUpperCase(char ch) {
+        return ch >= 'A' && ch <= 'Z';
+    }
+
+    private static boolean isLatinLetter(char ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+    }
+
+    private static boolean isUnderscore(char ch) {
+        return ch == '_';
+    }
+
+    private static boolean lastCharLetterOrDigit(char ch) {
+        return isLatinLetter(ch) || Character.isDigit(ch);
+    }
+
+    private static boolean isCorrectLength(String password) {
+        return password.length() >= 12 && password.length() <= 32;
+    }
+
+    public static void checkPassword(String password) {
+        if (!isCorrectLength(password)) {
             System.out.println("\"Пароль не соответствует требованиям!\"");
         }
-        if (Character.isLowerCase(password.charAt(0))){
+        if (!isLatinUpperCase(password.charAt(0))) {
             System.out.println("\"Пароль не соответствует требованиям!\"");
         }
-        if ()
+        if (!lastCharLetterOrDigit(password.charAt(password.length() - 1))) {
+            System.out.println("\"Пароль не соответствует требованиям!\"");
+        }
+        saveToFile(password);
 
     }
 
+    private static void saveToFile(String password) {
+        try (FileWriter file = new FileWriter("validPasswords.txt", true)) {
+            file.write(password + "\n");
+        } catch (IOException e) {
+            System.out.println("При сохренение файла возникла ошибка: " + e.getMessage());
+        }
+    }
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Введите пароль: ");
         String password = scan.nextLine();
 
+        scan.close();
 
-        if (password.isEmpty()){
-            System.out.println("\"Пароль не соответствует требованиям!\"");
+        if (password.isEmpty()) {
+            System.out.println("\"Пароль не может быть пустым!\"");
         } else {
             checkPassword(password);
         }
 
-        scan.close();
     }
 }
